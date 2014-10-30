1a192f6c3dd0a491d543c812130c28dd6d32cd37
Félix CARDON <felixcardon@free.fr>
2014-10-30 22:50:08 +0100

    Actualisation des températures intérieure et extérieure grâce aux données de la chaudière

---
+++ home/pi/domoticz/scripts/lua/script_time_viessmann.lua
@@ -0,0 +1,37 @@
+﻿- ingests tables: otherdevices,otherdevices_svalues
+-- 
+-- otherdevices and otherdevices_svalues are two item array for all devices: 
+--   otherdevices['yourotherdevicename']="On"
+--	otherdevices_svalues['yourotherthermometer'] = string of svalues
+--
+-- Based on your logic, fill the commandArray with device commands. Device name is case sensitive. 
+--
+-- Always, and I repeat ALWAYS start by checking for a state.
+-- If you would only specify commandArray['AnotherDevice']='On', every time trigger (e.g. every minute) will switch AnotherDevice on.
+--
+-- The print command will output lua print statements to the domoticz log for debugging.
+-- List all otherdevices states for debugging: 
+--   for i, v in pairs(otherdevices) do print(i, v) end
+-- List all otherdevices svalues for debugging: 
+--   for i, v in pairs(otherdevices_svalues) do print(i, v) end
+
+print('Mise à jour des device de la chaudière')
+
+local function getNumber(nom)
+	local handle = io.popen('vclient -h localhost:3002 -c '..nom..' | grep -E [0-9]+\\.[0-9]+ | cut -d " " -f 1')
+	local num = handle:read("*a")
+	handle:close()
+	return num
+end -- end getNumber
+
+local t1 = os.time()
+minutes = string.sub(t1, 15, 16)
+
+commandArray = {}
+
+if (string.sub(minutes, 1) == "5") then -- On met à jour les données toutes les cinq minutes
+	commandArray[1]={['UpdateDevice'] = '8|0|'..getNumber('getTempIntCC2')}
+	commandArray[1]={['UpdateDevice'] = '7|0|'..getNumber('getTempExt')}
+end 
+
+return commandArray
